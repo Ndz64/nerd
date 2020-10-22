9d5e245065cb5995c827c496cf744db07a1ba467
Ndz64 <67547317+Ndz64@users.noreply.github.com>
2020-10-22 20:56:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ var options =
 	},
 	identity: 
 	{
-		username: "knasz20",
-		password: "oauth:kq6c91aq3vdeg33ehn2nzrphyj7jg9",  
+		username: "ndsz",
+		password: "oauth:7ccik302y6gx0izhsxu967wb0zlb3o",  
 	},
-	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#vintagefps1', '#Umbrinquedo', ] //ADICIONA OS CANAIS AQ COM '#NOME'
+	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#vintagefps1', '#Umbrinquedo', '#velhovamp1' , ] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
 
 var client = new tmi.client(options);
